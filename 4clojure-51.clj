9b5cbc868e429730f32cbcefe4f4d6b602382064
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-23 22:07:58 +0100

    up tp 53

--- 4clojure-51.clj
+++ 4clojure-51.clj
@@ -5,3 +5,17 @@
 ; 52 Intro to Destructuring
 ; (= [2 4] (let [[a b c d e] [0 1 2 3 4]] __))
 [c e]
+
+; 53 Longest Increasing Sub-Seq
+; (= (__ [1 0 1 2 3 0 4 5]) [0 1 2 3])
+; (= (__ [5 6 1 3 2 7]) [5 6])
+; (= (__ [2 3 3 4 5]) [3 4 5])
+; (= (__ [7 6 5 4]) [])
+(fn [xs]
+  (letfn [(group-consecutive [acc val]
+                             (if (= (last (last acc)) (dec val))
+                               (reverse (conj (butlast acc) (conj (last acc) val)))
+                               (reverse (conj (reverse acc) (conj [] val)))))]
+    (concat
+      '() ; needed for the case where first returns nil
+      (first (reverse (sort-by count (filter #(>= (count %) 2) (reduce group-consecutive [] xs))))))))
