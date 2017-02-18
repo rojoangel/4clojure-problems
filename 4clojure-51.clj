098badf07fa30322e45d439d850b0ab9a6e3bf97
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-18 11:26:15 +0100

    up to 61

--- 4clojure-51.clj
+++ 4clojure-51.clj
@@ -128,3 +128,10 @@
    (lazy-seq (cons acc
                    (if xs    ;; stop sequencing
                      (my-reduction f (f acc (first xs)) (next xs)))))))
+
+; 61 Map Construction
+; (= (__ [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})
+; (= (__ [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})
+; (= (__ [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})
+(fn construct-map [ks vs]
+  (into (sorted-map) (map vector ks vs)))
