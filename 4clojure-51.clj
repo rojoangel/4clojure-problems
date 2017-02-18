1f0073cf4ff04b93526300b7d0f99197c528132a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-18 21:14:54 +0100

    up to 65

--- 4clojure-51.clj
+++ 4clojure-51.clj
@@ -164,3 +164,25 @@
 ; (=  0 (reduce __ []))
 ; (=  6 (reduce __ 1 [2 3]))
 +
+
+; 65 Black Box Testing
+; (= :map (__ {:a 1, :b 2}))
+; (= :list (__ (range (rand-int 20))))
+; (= :vector (__ [1 2 3 4 5 6]))
+; (= :set (__ #{10 (rand-int 5)}))
+; (= [:map :set :vector :list] (map __ [{} #{} [] ()]))
+; triky solution
+(fn black-box-testing [xs]
+  ({{} :map #{} :set }(empty xs)
+  (if (reversible? xs) :vector :list)))
+
+(fn black-box-testing [xs]
+  (cond
+   (= 2 (:m (conj xs [:m 1] [:m 2])))
+   :map
+   (= (inc (count xs)) (count (conj xs [:m 1] [:m 1])))
+   :set
+   (= [:m 2] (first (conj xs [:m 1] [:m 2])))
+   :list
+   (= [:m 2] (last (conj xs [:m 1] [:m 2])))
+   :vector))
