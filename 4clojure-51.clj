e64de6db2d97e27784e1369b606ee720f0ee0c82
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-18 12:29:07 +0100

    up to 63

--- 4clojure-51.clj
+++ 4clojure-51.clj
@@ -143,3 +143,18 @@
 (fn my-iterate [f x]
   (lazy-seq
    (cons x (my-iterate f (f x)))))
+
+; 63 Group a Sequence
+; (= (__ #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
+;(= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+;   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+;(= (__ count [[1] [1 2] [3] [1 2 3] [2 3]])
+;   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
+(fn my-group-by [f xs]
+  (reduce
+   (fn accumulate [m v]
+     (let [key (first v)
+           value (second v)]
+       (assoc m key (conj (get m key []) value))))
+   {}
+   (map vector (map f xs) xs)))
