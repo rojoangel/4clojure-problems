e437ccfb25888981f53bd460d0c58c62db3597da
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-19 16:16:45 +0100

    up to 69

--- 4clojure-51.clj
+++ 4clojure-51.clj
@@ -217,3 +217,22 @@
 ;      (recur (dec x) (conj result (+ 2 x)))
 ;      result)))
 [7 6 5 4 3]
+
+; 69 Merge with a Function
+; (= (__ * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+;   {:a 4, :b 6, :c 20})
+; (= (__ - {1 10, 2 20} {1 3, 2 10, 3 15})
+;   {1 7, 2 10, 3 15})
+; (= (__ concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+;   {:a [3 4 5], :b [6 7], :c [8 9]})
+(fn my-merge-with [f & ms]
+  (let [merge-entry (fn [m e]
+    (let [k (key e)
+          v (val e)]
+      (if (contains? m k)
+        (assoc m k (f (get m k) v))
+        (assoc m k v))))]
+    (reduce #(reduce merge-entry
+                     %1
+                     (map identity %2)) ; generates map-entries
+            ms)))
